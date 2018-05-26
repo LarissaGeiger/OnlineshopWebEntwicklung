bac40ab97a2ea8b7cefc4cd56b936e5c2c1a4da5
verenape <38526188+verenape@users.noreply.github.com>
2018-05-26 11:07:27 +0200

    Add files via upload

--- src/webshop/src/bean/AngeboteBean.java
+++ src/webshop/src/bean/AngeboteBean.java
@@ -5,9 +5,12 @@ import java.io.Serializable;
 public class AngeboteBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private String kategorieName;
+	private Integer produktID;
+
 
 	private String name;
-
+	private boolean angebot;
 	private Integer bildID;
 
 
@@ -29,6 +32,28 @@ public class AngeboteBean implements Serializable {
 		this.bildID = bildID;
 	}
 
-	
+	public String getKategorieName() {
+		return kategorieName;
+	}
+
+	public void setKategorieName(String kategorieName) {
+		this.kategorieName = kategorieName;
+	}
+
+	public Integer getProduktID() {
+		return produktID;
+	}
+
+	public void setProduktID(Integer produktID) {
+		this.produktID = produktID;
+	}
+
+	public boolean isAngebot() {
+		return angebot;
+	}
+
+	public void setAngebot(boolean angebot) {
+		this.angebot = angebot;
+	}
 
 }
--- src/webshop/src/bean/KontaktBean.java
+++ src/webshop/src/bean/KontaktBean.java
@@ -2,47 +2,62 @@ package bean;
 
 import java.io.Serializable;
 
-public class KontaktBean implements Serializable{
-	
+public class KontaktBean implements Serializable {
+
 	private static final long serialVersionUID = 1L;
+	private Integer id;
 	private String vorname;
 	private String nachname;
 	private String email;
 	private String usereingabe;
 	private String geschlecht;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getVorname() {
 		return vorname;
 	}
+
 	public void setVorname(String vorname) {
 		this.vorname = vorname;
 	}
+
 	public String getNachname() {
 		return nachname;
 	}
+
 	public void setNachname(String nachname) {
 		this.nachname = nachname;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getUsereingabe() {
 		return usereingabe;
 	}
+
 	public void setUsereingabe(String usereingabe) {
 		this.usereingabe = usereingabe;
 	}
+
 	public String getGeschlecht() {
 		return geschlecht;
 	}
+
 	public void setGeschlecht(String geschlecht) {
 		this.geschlecht = geschlecht;
 	}
-	
-	
-	
-	
-	
+
 }
